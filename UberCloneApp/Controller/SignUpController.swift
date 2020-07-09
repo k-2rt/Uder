3bbb72e6b20541cdcfbbb9b95cfde8ab0857188f
鶴田和眞 <k.tsuruta.1108@gmail.com>
2020-07-09 23:57:47 +0900

    First init

---
+++ UberCloneApp/Controller/SignUpController.swift
@@ -0,0 +1,22 @@
+//
+//  SignUpController.swift
+//  UberCloneApp
+//
+//  Created by MacBook Pro on 2020/07/09.
+//  Copyright © 2020 Tsurutadesu. All rights reserved.
+//
+
+import UIKit
+
+class SignUpController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+    
+    // MARK: - Selectors
+}
