847dd2f462f2c73252a7bd3ff4a07069f2dbebd5
鶴田和眞 <k.tsuruta.1108@gmail.com>
2020-07-24 01:21:55 +0900

    Create Side Menu & Add log out Action

---
+++ UberCloneApp/View/MenuHeader.swift
@@ -0,0 +1,73 @@
+//
+//  MenuHeader.swift
+//  UberCloneApp
+//
+//  Created by MacBook Pro on 2020/07/22.
+//  Copyright © 2020 Tsurutadesu. All rights reserved.
+//
+
+import UIKit
+
+class MenuHeader: UIView {
+    
+    // MARK: - Properties
+    
+    private let user: User
+    
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .lightGray
+        return iv
+    }()
+    
+    private lazy var fullnameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .white
+        label.text = "Kazuma Tsuruta"
+        label.text = user.fullname
+        return label
+    }()
+    
+    private lazy var emailLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .lightGray
+        label.text = "test@gmail.com"
+        label.text = user.email
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
+    init(user: User, frame: CGRect) {
+        self.user = user
+        super.init(frame: frame)
+        
+        backgroundColor = .backgroundColor
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor,
+                            paddingTop: 4, paddingLeft: 12,
+                            width: 64, height: 64)
+        profileImageView.layer.cornerRadius = 64 / 2
+        
+        let stack = UIStackView(arrangedSubviews: [fullnameLabel, emailLabel])
+        stack.distribution = .fillEqually
+        stack.spacing = 4
+        stack.axis = .vertical
+        
+        addSubview(stack)
+        stack.centerY(inView: profileImageView,
+                     leftAnchor: profileImageView.rightAnchor,
+                     paddingLeft: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Selectors
+
+    // MARK: - Helper Functions
+}
