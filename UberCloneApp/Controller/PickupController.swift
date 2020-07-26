2951e061fefc2f70af47b9bcc808b9978a3c1393
鶴田和眞 <k.tsuruta.1108@gmail.com>
2020-07-27 00:13:25 +0900

    Comment out

--- UberCloneApp/Controller/PickupController.swift
+++ UberCloneApp/Controller/PickupController.swift
@@ -93,9 +93,9 @@ class PickupController: UIViewController {
     @objc func animateProgress() {
         circularProgressView.animatePulsatingLayer()
         circularProgressView.setProgressWithAnimation(duration: 5, value: 0) {
-            DriverService.shared.updateTripState(trip: self.trip, state: .denied) { (err, ref) in
-                self.dismiss(animated: true, completion: nil)
-            }
+//            DriverService.shared.updateTripState(trip: self.trip, state: .denied) { (err, ref) in
+//                self.dismiss(animated: true, completion: nil)
+//            }
         }
     }
     
